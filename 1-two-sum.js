441e4b28f4ba8ef78e64fb165f653a082be2cf86
Himel Das <himelhub@gmail.com>
2020-02-07 00:55:26 +0600

    Update 1-two-sum.js

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -23,9 +23,9 @@ if current element's complement already exists in the table.
 If it exists, we have found a solution and return immediately.
 
 Complexity Analysis:
-Time complexity : O(n). We traverse the list containing nn elements only once.
+Time complexity : O(n). We traverse the list containing n elements only once.
 Each look up in the table costs only O(1) time.
 
 Space complexity : O(n). The extra space required depends on the number of items stored in the hash table,
-which stores at most nn elements.
+which stores at most n elements.
 */
