ef37d79001e682bf56f2f46c6d3fbc092d4926e5
Himel Das <himelhub@gmail.com>
2020-02-13 17:31:21 +0600

    Update 1-two-sum.js

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -5,6 +5,8 @@ https://leetcode.com/problems/two-sum/
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
+Bonus: Return 'No solution' if there is no solution.
+
 Example:
 Given nums = [2, 7, 11, 15], target = 9,
 
@@ -14,7 +16,7 @@ return [0, 1].
 */
 
 const nums = [12,15,19,20,2,3,9,26,55,63];
-const target = 67;
+const target = 670;
 
 var twoSum = function(nums, target) {
     var map = new Map;
@@ -25,6 +27,7 @@ var twoSum = function(nums, target) {
         }
         map.set(nums[i], i);
     }
+    return 'No solution'; //if there is no solution the control will come here and return this
 };
 
 console.log(twoSum(nums, target));
